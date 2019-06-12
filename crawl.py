7722efaede67133a5334d5584c8d071a197717b3
Dusan Radisavljevic <ducaradisavljevic@gmail.com>
2019-06-12 14:52:37 +0900

    Add crawler. Modify scraper.

---
+++ crawl.py
@@ -0,0 +1,96 @@
+import bs4
+import csv
+import os
+from selenium import webdriver
+import time
+import configuration as cfg
+
+def main():
+    scraped_leagues = []
+    options = webdriver.ChromeOptions()
+    options.add_argument('--ignore-certificate-errors')
+    options.add_argument('--incognito')
+    #options.add_argument('--headless')
+    driver = webdriver.Chrome(os.path.join(os.getcwd(), cfg.driver_name), options=options)
+    level = 1
+    offset = 0
+
+    link = cfg.root_link+'/league/3855'
+    league_link = '/league/3855'
+    while level != 4:
+        print(level)
+        league_tabs, selenium_tabs, scraped_leagues = crawl(driver, link, scraped_leagues, league_link)
+        for i in range(0,len(league_tabs)):
+            next_league = league_tabs[i].find('a')
+            selenium_tabs[i].click()
+            soup = get_soup(driver)
+            league_container = soup.find('div', {'class': 'tab-content'})
+            crawl_league_container = league_container.find('div', {'class': 'active'})
+            leagues = crawl_league_container.find_all('a')
+            exit = False
+            for league in leagues:
+                if league['href'].split('/')[2] not in scraped_leagues:
+                    exit = True
+                    if next_league.text.strip().replace('"','')=='Niži rang':
+                        if offset == 0:
+                            level = level + 1
+                        else:
+                            offset = offset - 1
+                    elif next_league.text.strip().replace('"','')=='Liga višeg ranga':
+                        offset = offset + 1
+                    league_link=league['href']
+                    link = cfg.root_link+league_link
+                    break
+            if exit:
+                break
+            elif i == len(league_tabs)-1:
+                league_link, link = dead_end_prevention(league_tabs, selenium_tabs, driver)
+    level = 1
+    driver.close()
+
+def dead_end_prevention(league_tabs, selenium_tabs, driver):
+    for i in range(0,len(league_tabs)):
+        next_league = league_tabs[i].find('a')
+        if next_league.text.strip().replace('"','')=='Ostala takmičenja':
+            selenium_tabs[i].click()
+            break
+    soup = get_soup(driver)
+    league_container = soup.find('div', {'class': 'tab-content'})
+    crawl_league_container = league_container.find('div', {'class': 'active'})
+    leagues = crawl_league_container.find_all('a')
+    exit = False
+    for league in leagues:
+        driver.get(cfg.root_link+league['href'])
+        soup = get_soup(driver)
+        league_list = soup.find('div',{'class': 'league-nav'})
+        league_tabs = league_list.find_all('li', {'role': 'presentation'})
+        for i in range(0,len(league_tabs)):
+            next_league = league_tabs[i].find('a')
+            if next_league.text.strip().replace('"','')=='Niži rang':
+                league_link=league['href']
+                link = cfg.root_link+league_link
+                exit = True
+                break
+        if exit:
+            break
+    return league_link, link
+
+
+def crawl(driver, link, scraped_leagues, league_link):
+    driver.get(link)
+    soup = get_soup(driver)
+    print(soup.find('h1', {'class': 'page-name'}).text.strip().replace('"',''))
+    scraped_leagues.append(league_link.split('/')[2])
+    league_list = soup.find('div',{'class': 'league-nav'})
+    league_tabs = league_list.find_all('li', {'role': 'presentation'})
+    selenium_tabs = driver.find_elements_by_xpath('//*/a[@role="tab"]')
+    return league_tabs, selenium_tabs, scraped_leagues
+
+
+def get_soup(driver):
+    page_source = driver.page_source
+    soup = bs4.BeautifulSoup(page_source, 'lxml')
+    return soup
+
+if __name__ == '__main__':
+    main()
--- fssArchivator.py
+++ fssArchivator.py
@@ -21,17 +21,7 @@ def main():
     options.add_argument('--headless')
     driver = webdriver.Chrome(os.path.join(os.getcwd(), cfg.driver_name), options=options)
 
-    football_clubs, identifier = append_to_csv(cfg.superliga_start_page, driver, 1, football_clubs, identifier)
-    football_clubs, identifier = append_to_csv(cfg.prva_liga_start_page, driver, 2, football_clubs, identifier)
-    football_clubs, identifier = append_to_csv(cfg.srpska_liga_vojvodina_start_page, driver, 3, football_clubs, identifier)
-    football_clubs, identifier = append_to_csv(cfg.srpska_liga_zapad_start_page, driver, 3, football_clubs, identifier)
-    football_clubs, identifier = append_to_csv(cfg.srpska_liga_istok_start_page, driver, 3, football_clubs, identifier)
-    football_clubs, identifier = append_to_csv(cfg.srpska_liga_beograd_start_page, driver, 3, football_clubs, identifier)
-
-    driver.close()
-
-def append_to_csv(source, driver, league_level, football_clubs, identifier):
-    driver.get(source)
+    driver.get(cfg.superliga_start_page)
     page_source = driver.page_source
     soup = bs4.BeautifulSoup(page_source, 'lxml')
 
@@ -43,60 +33,60 @@ def append_to_csv(source, driver, league_level, football_clubs, identifier):
         for j in range(0,len(season_links)):
             if season_links[j].text.strip().replace('"','')=='2005-2006':
                 break
-            league_season = season_links[j].text.strip().replace('"','')
-            driver.get(cfg.root_link+season_links[j]['href'])
-            game_buttons = driver.find_elements_by_class_name('page-link')
-            driver.execute_script('arguments[0].click();', game_buttons[0])
-            page_source = driver.page_source
-            soup = bs4.BeautifulSoup(page_source, 'lxml')
-            league_name = soup.find('h1', {'class': 'page-name'}).text.strip().replace('"','')
-            matchday = 1
-
-            matchday_selector = soup.find('select', {'id': 'kolo'})
-            for i in range(0,len(matchday_selector.find_all('option'))):
-                time.sleep(1)
-                game_buttons = driver.find_elements_by_class_name('page-link')
-                driver.execute_script('arguments[0].click();', game_buttons[2])
-                page_source = driver.page_source
-                soup = bs4.BeautifulSoup(page_source, 'lxml')
-
-                match_selector = soup.find_all('tr', {'class': 'result-row'})
-
-                for match in match_selector:
-                    match_date = match.find('a', {'class': 'game-date'}).text.strip().replace('"','')
-                    match_time = match.find('span', {'class': 'game-time'}).text.strip().replace('"','')
-                    host = match.find('td', {'class': 'team-host'})
-                    host_link = host.find('a')
-                    host_name = host_link.text
-                    host_url = cfg.root_link+host_link['href']
-                    host_city = host_link['data-original-title']
-                    host_site_id = host_url.split('/club/')[1].split('-')[0]
-                    if host_site_id not in football_clubs:
-                        football_clubs[host_site_id] = identifier
-                        identifier = identifier + 1
-                    host_id = football_clubs[host_site_id]
-                    guest = match.find('td', {'class': 'team-guest'})
-                    guest_link = guest.find('a')
-                    guest_name = guest_link.text
-                    guest_url = cfg.root_link+guest_link['href']
-                    guest_city = guest_link['data-original-title']
-                    guest_site_id = guest_url.split('/club/')[1].split('-')[0]
-                    if guest_site_id not in football_clubs:
-                        football_clubs[guest_site_id] = identifier
-                        identifier = identifier + 1
-                    guest_id = football_clubs[guest_site_id]
-                    goals_host = match.find('span', {'class': 'res-1'}).text.strip().replace('"','')
-                    if not goals_host:
-                        goals_host = '0'
-                    goals_guest = match.find('span', {'class': 'res-2'}).text.strip().replace('"','')
-                    if not goals_guest:
-                        goals_guest = '0'
-                    row = [league_name, league_level, league_season, matchday, match_date, match_time, host_id, host_name, host_city, host_url, guest_id, guest_name, guest_city, guest_url, goals_host, goals_guest]
-                    writer.writerow(row)
-                    print('Writing :' + league_name + ' ' + league_season + ' matchday ' + matchday)
-                matchday = matchday+1
-
+            football_clubs, identifier = scrape_match(season_links[j], driver, 1, football_clubs, identifier, writer)
     csvFile.close()
+
+    driver.close()
+
+def scrape_match(link, driver, league_level, football_clubs, identifier, writer):
+    league_season = link.text.strip().replace('"','')
+    driver.get(cfg.root_link+link['href'])
+    game_buttons = driver.find_elements_by_class_name('page-link')
+    driver.execute_script('arguments[0].click();', game_buttons[0])
+    page_source = driver.page_source
+    soup = bs4.BeautifulSoup(page_source, 'lxml')
+    league_name = soup.find('h1', {'class': 'page-name'}).text.strip().replace('"','')
+    matchday = 1
+
+    matchday_selector = soup.find('select', {'id': 'kolo'})
+    for i in range(0,len(matchday_selector.find_all('option'))):
+        time.sleep(1)
+        game_buttons = driver.find_elements_by_class_name('page-link')
+        driver.execute_script('arguments[0].click();', game_buttons[2])
+        page_source = driver.page_source
+        soup = bs4.BeautifulSoup(page_source, 'lxml')
+
+        match_selector = soup.find_all('tr', {'class': 'result-row'})
+
+        for match in match_selector:
+            match_date = match.find('a', {'class': 'game-date'}).text.strip().replace('"','')
+            match_time = match.find('span', {'class': 'game-time'}).text.strip().replace('"','')
+            host = match.find('td', {'class': 'team-host'})
+            host_link = host.find('a')
+            host_name = host_link.text
+            host_url = cfg.root_link+host_link['href']
+            host_city = host_link['data-original-title']
+            host_site_id = host_url.split('/club/')[1].split('-')[0]
+            if host_site_id not in football_clubs:
+                football_clubs[host_site_id] = identifier
+                identifier = identifier + 1
+            host_id = football_clubs[host_site_id]
+            guest = match.find('td', {'class': 'team-guest'})
+            guest_link = guest.find('a')
+            guest_name = guest_link.text
+            guest_url = cfg.root_link+guest_link['href']
+            guest_city = guest_link['data-original-title']
+            guest_site_id = guest_url.split('/club/')[1].split('-')[0]
+            if guest_site_id not in football_clubs:
+                football_clubs[guest_site_id] = identifier
+                identifier = identifier + 1
+            guest_id = football_clubs[guest_site_id]
+            goals_host = match.find('span', {'class': 'res-1'}).text.strip().replace('"','')
+            goals_guest = match.find('span', {'class': 'res-2'}).text.strip().replace('"','')
+            row = [league_name, league_level, league_season, matchday, match_date, match_time, host_id, host_name, host_city, host_url, guest_id, guest_name, guest_city, guest_url, goals_host, goals_guest]
+            writer.writerow(row)
+            print('Writing: ' + league_name + ' ' + league_season + ' matchday ' + str(matchday))
+        matchday = matchday + 1
     return football_clubs, identifier
 
 
